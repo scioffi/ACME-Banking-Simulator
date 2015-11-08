2dbe75c456df4832812e048cf0f4e5fead46fe03
Michael <michael.incardona@outlook.com>
2015-11-08 13:16:43 -0500

    Added comments

--- Bank.java
+++ Bank.java
@@ -1,5 +1,7 @@
 /**
- * The main Bank class
+ * Represents a logical Bank.
+ * @author Michael Incardona mji8299
+ * @author Steven Cioffi
  */
 public class Bank {
     public static void main(String[] args) {
