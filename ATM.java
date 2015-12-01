463922e236273ba64c1a5f5fefa3f4a39010486b
Stephen Cioffi <stephen@stephencioffi.com>
2015-11-30 20:34:47 -0500

    Added JavaDocs to ATM

--- ATM.java
+++ ATM.java
@@ -13,6 +13,11 @@ public class ATM extends Observable implements Observer {
         bank = b;
     }
 
+    /**
+     * Verify whether or not account ID exists.
+     * @param id numerical account ID.
+     * @return Whether or not account ID exists.
+     */
     public boolean validateID(String id) {
         Account a = bank.getAccount(id);
         if (a == null) {
@@ -25,31 +30,55 @@ public class ATM extends Observable implements Observer {
         }
     }
 
+    /**
+     * Verify whether the PIN is correct.
+     * @param pin PIN number to be verified.
+     * @return Whether or not the PIN matches the account.
+     */
     public boolean validatePIN(String pin) {
         return account.matchesPIN(pin);
     }
 
+    /**
+     * Remove all observers and terminate the ATM session.
+     */
     public void closeAll() {
         this.deleteObservers();
         close();
     }
 
+    /**
+     * Close window and delete instance of ATM.
+     */
     public void close() {
         if (account == null)
             return;
         account.deleteObserver(this);
         account = null;
     }
-    
+
+    /**
+     * Update the ATM and notify observers.
+     */
     private void triggerUpdate() {
         this.setChanged();
         this.notifyObservers();
     }
 
+    /**
+     * Add dollar sign in front of cash.
+     * @param numbers money.
+     * @return new String containing a dollar sign in front of money.
+     */
     public static String formatCash(String numbers) {
         return "$" + digitsToCash(numbers);
     }
-    
+
+    /**
+     * Convert numerical value to a money syntax.
+     * @param numbers numerical value.
+     * @return Syntax.
+     */
     public static String digitsToCash(String numbers) {
         String temp = "";
         if (numbers.length() == 0) {
@@ -66,10 +95,20 @@ public class ATM extends Observable implements Observer {
         return temp;
     }
 
+    /**
+     * Deposit funds into the account.
+     * @param cash Amount of money to deposit.
+     * @return boolean result of deposit success.
+     */
     public boolean deposit(double cash) {
         return account.deposit(cash);
     }
-    
+
+    /**
+     * Withdraw funds from the account.
+     * @param cash Amount of money to withdraw.
+     * @return boolean result of withdraw success.
+     */
     public boolean withdraw(double cash) {
         if (account instanceof Withdrawable) {
             return ((Withdrawable)(account)).withdraw(cash);
@@ -77,6 +116,10 @@ public class ATM extends Observable implements Observer {
         return false;
     }
 
+    /**
+     * Return the balance of the account.
+     * @return account balance.
+     */
     public double balance() {
         return account.getBalance();
     }
